94a7badaebb888b59af2ebe15f6fb16e0409bbca
koba <koba@growal.com>
2022-04-09 06:16:47 +0000

    r

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
   </header>
 
   <main></main>
@@ -58,6 +69,12 @@ a,
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
 
   .logo {
