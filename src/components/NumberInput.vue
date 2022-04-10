6605f1d32c547a8318dbe9fcc51472335c1ac539
koba <koba@growal.com>
2022-04-10 08:57:45 +0000

    n

---
+++ src/components/NumberInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input :value="modelValue" @input="emitValue" type="number" />
+</template>
+
+<script lang="ts">
+  export default {
+    props: {
+      modelValue: {
+        type: Number
+      },
+      modelModifiers: {}
+    },
+    methods: {
+      emitValue({ target: { value } }) {
+        if (this.modelModifiers.numberOnly && value === '') {
+          value = 0;
+        }
+
+        this.$emit('update:modelValue', Number(value));
+      }
+    }
+  }
+</script>
