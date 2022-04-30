3090447d01b0a51676441b193b4864f4fcc32d24
koba <koba@growal.com>
2022-04-30 08:32:24 +0000

    s

--- src/components/buttons/ButtonSample.vue
+++ src/components/buttons/ButtonSample.vue
@@ -4,16 +4,34 @@
     <div class="mb-2">
       <PrimaryButton @click="handleClick">Primary</PrimaryButton>
     </div>
+    <div class="mb-2">
+      <PrimaryButton :disabled="true" @click="handleClick">
+        Primary Disabled
+      </PrimaryButton>
+    </div>
+    <div class="mb-2">
+      <SecondaryButton @click="handleClick">Secondary</SecondaryButton>
+    </div>
+    <div class="mb-2">
+      <SecondaryButton
+        :disabled="true"
+        @click="handleClick"
+      >
+        Secondary Disabled
+      </SecondaryButton>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import PrimaryButton from './PrimaryButton.vue';
+import SecondaryButton from './SecondaryButton.vue';
 
 export default defineComponent({
   components: {
     PrimaryButton,
+    SecondaryButton
   },
   emits: ['click'],
   setup() {
---
+++ src/components/buttons/SecondaryButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <BaseButton
+    :disabled="disabled"
+    :class="buttonClass"
+    @click="handleClick"
+  >
+    <slot />
+  </BaseButton>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import BaseButton from './BaseButton.vue';
+
+export default defineComponent({
+  components: {
+    BaseButton,
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: {
+    click: null,
+  },
+  setup(props, { emit }) {
+    const handleClick = () => {
+      emit('click');
+    };
+
+    const buttonClass = computed(() =>
+    props.disabled ? 'text-gray-400 bg-gray-100' : 'text-gray-900 bg-gray-200');
+
+    return {
+      handleClick,
+      buttonClass
+    }
+  }
+});
+</script>
