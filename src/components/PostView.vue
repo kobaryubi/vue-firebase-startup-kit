8edf4dcd023c7cce50e14f695b51bd7e01654da6
koba <koba@growal.com>
2022-05-05 08:59:21 +0000

    p

---
+++ src/components/PostView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    v-for="post in data"
+    :key="post.id"
+  >
+    <p class="py-2 border-b-4">
+      {{ post.id }}:{{ post.title }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import axios from 'axios';
+
+type Post = {
+  usearId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
+
+export default defineComponent({
+  async setup() {
+    const { data } = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+
+    return {
+      data
+    };
+  }
+});
+</script>
