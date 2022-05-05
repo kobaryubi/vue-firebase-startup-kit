17fe247c51839b07f6206677f3d2b34aaec7d72c
koba <koba@growal.com>
2022-05-05 09:59:50 +0000

    msw

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
+import { worker } from './mocks/browser';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 createApp(App).mount('#app');
