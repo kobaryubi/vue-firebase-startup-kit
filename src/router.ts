487b1f7249852c01d0e4170c1149c21d78bc1acc
koba <koba@growal.com>
2022-05-14 13:58:24 +0000

    c

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import HomeView from '@/views/HomeView.vue';
 import TodosView from '@/views/TodosView.vue';
+import CreateTodoView from '@/views/CreateTodoView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/todos', component: TodosView },
+  { path: '/todos/create', component: CreateTodoView }
 ];
 
 export const router = createRouter({
