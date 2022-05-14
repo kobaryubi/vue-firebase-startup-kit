e0d9b50c8db5de0b1f58c08e0cb5a18c3c7ab1d1
koba <koba@growal.com>
2022-05-14 07:30:30 +0000

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <main />
+  <main>
+    <h1>TODO List</h1>
+
+    <p>{{ message }}</p>
+    <p>Count is: {{ counter.count }}</p>
+  </main>
 </template>
 
-<script lang="ts" />
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const counter = reactive({ count: 0 });
+const message = ref('Hello World!');
+</script>
