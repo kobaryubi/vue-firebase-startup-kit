03e6824d09cc0d25bad54f24d3cc8ab7cf7cc6a8
koba <koba@growal.com>
2022-05-15 07:41:01 +0000

    u

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,23 @@
 <template>
-  <p>HomeView</p>
+  <div>
+    <h2>Home</h2>
+    <div id="firebaseui-auth-container" />
+    <div id="loader">
+      Loading...
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ui } from '@/firebase';
+import { EmailAuthProvider } from 'firebase/auth';
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    {
+      provider: EmailAuthProvider.PROVIDER_ID,
+      signInMethod: EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+    }
+  ],
+});
+</script>
