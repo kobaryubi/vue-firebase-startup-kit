632cccf46708fd68da8235c18ee79eda88028b27
koba <koba@growal.com>
2022-05-15 07:17:40 +0000

    f

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,19 @@
-import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { initializeApp, getApps } from 'firebase/app';
+import * as firebaseui from 'firebaseui';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.emv.VITE_FIREBASE_PROJECT_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-initializeApp(firebaseConfig);
+
+if (getApps().length === 0) {
+  initializeApp(firebaseConfig);
+}
+
+const auth = getAuth();
+
+export const ui = new firebaseui.auth.AuthUI(auth);
