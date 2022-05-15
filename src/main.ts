a490f187962816624fed7f4bac5da7b106d5db71
koba <koba@growal.com>
2022-05-15 01:00:40 +0000

    m

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
-import { worker } from './mocks/browser';
 import { router } from '@/router';
-
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
 
 const app = createApp(App);
 app.use(router);
