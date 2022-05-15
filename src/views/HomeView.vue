100625e1a125587022f0054362b9e03ef970577e
koba <koba@growal.com>
2022-05-15 11:37:39 +0000

    u

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,14 +9,12 @@
 import { ui } from '@/firebase';
 import { EmailAuthProvider } from 'firebase/auth';
 
-if (ui.isPendingRedirect()) {
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      {
-        provider: EmailAuthProvider.PROVIDER_ID,
-        signInMethod: EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
-      }
-    ],
-  });
-}
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    {
+      provider: EmailAuthProvider.PROVIDER_ID,
+      signInMethod: EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+    }
+  ],
+});
 </script>
